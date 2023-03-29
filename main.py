0ae67f3f471857ebd826c867193e65276ed82578
akmhmgc <akimuhamaguchi@icloud.com>
2023-03-29 23:21:51 +0900

    Prompt templateを使用

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from dotenv import load_dotenv
 import os
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+
+prompt = PromptTemplate(
+    input_variables=["product"],
+    template="What is a good name for a company that makes {product}?",
+)
 
 # .envファイルをロード
 load_dotenv()
@@ -11,4 +17,4 @@ openai_api_key = os.getenv('OPENAI_API_KEY')
 llm = OpenAI(temperature=0.9, openai_api_key=openai_api_key)
 
 text = "What would be a good company name for a company that makes colorful socks?"
-print(llm(text))
+print(llm(prompt.format(product="colorful socks")))
